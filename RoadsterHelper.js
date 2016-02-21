9e96d7f293c26ef1b21d83cd9cf8eda4aeccf992
ganonscrub <mboharsik@gmail.com>
2016-02-21 00:06:51 -0500

    Incremented to 0.2

--- RoadsterHelper.js
+++ RoadsterHelper.js
@@ -1,13 +1,16 @@
 // ==UserScript==
 // @name         Roadster Helper
 // @namespace    ganonscrub_script
-// @version      0.1
+// @version      0.2
 // @description  Makes RoadsterRoutes slightly less bad
 // @author       ganonscrub
 // @include      *uml.edu/routes/*
 // @grant        none
 // @updateURL	 https://raw.githubusercontent.com/ganonscrub/RoadsterHelper/master/RoadsterHelper.js
 // ==/UserScript==
+
+var expires = new Date();
+expires.setYear( expires.getYear() + 1900 + 1 );
 
 document.getElementsByClassName( "content" )[0].className = "content accordian";
 
@@ -21,7 +24,7 @@ function hideNumbers(){
 
 function shouldShowNumbers(){
     if ( document.cookie.indexOf( "showNumbers" ) == -1 ){
-        document.cookie = "showNumbers=false";
+        document.cookie = "showNumbers=false; expires=" + expires;
         return false;
     }
     else{
@@ -35,6 +38,7 @@ function shouldShowNumbers(){
 }
 
 function makeAccordianScrollable(){
+    var acc = $("#accordian");
     var box = $(".content.accordian");
     if ( window.innerHeight < 533 ){
         //console.log( box.height(), window.innerHeight );
@@ -42,33 +46,59 @@ function makeAccordianScrollable(){
         box.css( { overflow:"scroll" } );
     }
     else{
-        box.height( 543 );
-        $("#accordian").width( 220 );
+        //box.height( 543 );
+        box.height( acc.height() );
+        acc.width( 220 );
         box.css( { overflow:"visible" } );
     }
 }
 
 function cookieToggle(){
-    var checkboxes = $("[name=busLabelsToggle]");
-    var checked = checkboxes.prop( "checked" );
-    console.log( "checked returned " + checked.toString() );
-    if ( checked )
-        document.cookie = "showNumbers=true";
-    else
-        document.cookie = "showNumbers=false";
+    var toggle = $("#permNumbersToggle");
+    if ( shouldShowNumbers() ){
+        toggle.prop( "innerHTML", "NUMBERS OFF" );
+        toggle.css( { "color": "crimson" } );
+        
+        document.cookie = "showNumbers=false; expires=" + expires;
+        hideNumbers();
+    }
+    else{
+        toggle.prop( "innerHTML", "NUMBERS ON" );
+        toggle.css( { "color": "chartreuse" } );
+        document.cookie = "showNumbers=true; expires=" + expires;
+        showNumbers();
+    }
+}
+
+function bannerResize(){
+    var toggle = $("#permNumbersToggle");    
+    
+    toggle.css( { "margin-left": window.innerWidth / 2 } );
 }
 
 $(document).ready( function(){
  
+    $("body").append( "<div id='permNumbersToggle'></div>" );
+    var toggle = $("#permNumbersToggle");    
+    if ( shouldShowNumbers() ){
+        toggle.prop( "innerHTML", "NUMBERS ON" );
+        toggle.css( { "color": "chartreuse" } );
+    }
+    else{
+        toggle.prop( "innerHTML", "NUMBERS OFF" );
+        toggle.css( { "color": "crimson" } );
+    }    
+    toggle.css( { "position": "absolute", "width": "auto", "margin-left": window.innerWidth / 2, "margin-top": "10px", "font-size": "16px", "font-weight": "bold", "z-index": 1, "cursor": "pointer" } );
+    toggle.click( cookieToggle );
+    
+    openMoreInfo();
+    closeMoreInfo();
+    
     if ( shouldShowNumbers() )
         showNumbers();
     
-    makeAccordianScrollable();
-    
 	$(window).resize( makeAccordianScrollable );
+    $(window).resize( bannerResize );
     
-    document.getElementsByClassName( "button" )[5].addEventListener( function() {
-        $("[name=busLabelsToggle]").click( cookieToggle );
-    });
-    
+    setTimeout( makeAccordianScrollable, 1000 );    
 });
